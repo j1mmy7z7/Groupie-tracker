b91b411dc0967226bcf37d8efed1f2416d72826c
abrakingoo <maikoabraham68@gmail.com>
2024-08-10 15:21:17 +0300

    remove: remove unused handlers

--- cmd/main.go
+++ cmd/main.go
@@ -9,9 +9,7 @@ import (
 func main() {
 
 	http.HandleFunc("/", handlers.Homehandler)
-	http.HandleFunc("/location", handlers.Locationhandler)
-	http.HandleFunc("/dates", handlers.Dateshandler)
-	http.HandleFunc("/relation", handlers.Relationshandler)
+	http.HandleFunc("/display", handlers.Locationhandler)
 
 	log.Println("running on 8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
