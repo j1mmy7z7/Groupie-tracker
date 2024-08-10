398e4050672d2c751ae03443ee2823a1a8a1cc79
abrakingoo <maikoabraham68@gmail.com>
2024-08-10 23:03:02 +0300

    refactor: Enhance template rendering and error handling in Homehandler
    - Introduced a `Rendertemplate` function to centralize template rendering, improving code reuse and consistency.
    - Added structured error responses using `data.PageData` to replace plain `http.Error` calls.
    - Improved the initialization process by loading all templates during startup and handling any parsing errors.
    - Ensured that `Homehandler` provides meaningful feedback through templated error pages when data fetching or response reading fails.

--- handlers/home.go
+++ handlers/home.go
@@ -33,25 +33,74 @@ func Homehandler(w http.ResponseWriter, r *http.Request) {
 
 	resp, err := http.Get(url)
 	if err != nil {
-		http.Error(w, "error fetching data", http.StatusInternalServerError)
+		data := data.PageData{
+			Title: "Error",
+			Bands: struct {
+				Message string
+				Code    int
+			}{
+				Message: "Error fetching data",
+				Code:    500,
+			},
+		}
+		w.WriteHeader(http.StatusInternalServerError)
+		Rendertemplate(w, data)
 		return
 	}
 
 	defer resp.Body.Close()
 	// Check for successful response
 	if resp.StatusCode != http.StatusOK {
-		http.Error(w, "Unexpected status code from external service", http.StatusInternalServerError)
+		data := data.PageData{
+			Title: "Error",
+			Bands: struct {
+				Message string
+				Code    int
+			}{
+				Message: "Unexpected status code from external service",
+				Code:    500,
+			},
+		}
+		w.WriteHeader(http.StatusInternalServerError)
+		Rendertemplate(w, data)
 		return
+
 	}
 
 	resBody, err := io.ReadAll(resp.Body)
 	if err != nil {
-		http.Error(w, "Error reading reponse data", http.StatusInternalServerError)
+		data := data.PageData{
+			Title: "Error",
+			Bands: struct {
+				Message string
+				Code    int
+			}{
+				Message: "Error reading response data",
+				Code:    500,
+			},
+		}
+		w.WriteHeader(http.StatusInternalServerError)
+		Rendertemplate(w, data)
 		return
 	}
 	var Bandis []data.Band
 
-	json.Unmarshal(resBody, &Bandis)
+	err = json.Unmarshal(resBody, &Bandis)
+	if err != nil {
+		data := data.PageData{
+			Title: "Error",
+			Bands: struct {
+				Message string
+				Code    int
+			}{
+				Message: "Error Unmarshaling data",
+				Code:    500,
+			},
+		}
+		w.WriteHeader(http.StatusInternalServerError)
+		Rendertemplate(w, data)
+		return
+	}
 
 	data := data.PageData{
 		Title: "Home",
