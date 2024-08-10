80f1ae8efab62709c2d42ebeb5ee72d31542007c
jmuchiri <mmjames451@gmail.com>
2024-08-10 14:23:05 +0300

    feat: add handler for relations route

--- cmd/main.go
+++ cmd/main.go
@@ -11,6 +11,7 @@ func main() {
 	http.HandleFunc("/", handlers.Homehandler)
 	http.HandleFunc("/location", handlers.Locationhandler)
 	http.HandleFunc("/dates", handlers.Dateshandler)
+	http.HandleFunc("/relation", handlers.Relationshandler)
 
 	log.Println("running on 8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
