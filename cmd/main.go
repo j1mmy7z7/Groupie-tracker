9e11ff6f36e4a062b71b34165d6fed9c323eb1d6
abrakingoo <maikoabraham68@gmail.com>
2024-08-12 12:08:07 +0300

    feat: add handler for static

--- cmd/main.go
+++ cmd/main.go
@@ -8,7 +8,7 @@ import (
 )
 
 func main() {
-
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		path := r.URL.Path
 
